47d3cc1d0fe5eebcfc1b4ec002f9d508df06aabc
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-16 21:23:12 +0300

    Initial Commit

---
+++ Beden Kitle Endexsi Hesaplayicisi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beden Kitle Endexsi Hesaplayicisi
+//
+//  Created by Aytaç Bulanık on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
