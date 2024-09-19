96d9d6e60cbf0b8b47c92d402bc7cee90c58d942
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-19 22:08:54 +0300

    secondViewController added

---
+++ Beden Kitle Endexsi Hesaplayicisi/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Beden Kitle Endexsi Hesaplayicisi
+//
+//  Created by Aytaç Bulanık on 19.09.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
